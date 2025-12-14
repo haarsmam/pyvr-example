65f75ea70d67693a208fa78e95534b21aea8d1ef
Marc Haarsma <marc.haarsma@gmail.com>
2025-12-14 15:49:23 +0100

    format

--- mgllib/mat3d.py
+++ mgllib/mat3d.py
@@ -1,6 +1,7 @@
 import numpy as np
 import glm
 
+
 class Transform3D:
     def __init__(self, swap_rot_order=True):
         self.pos = [0, 0, 0]
@@ -18,7 +19,7 @@ class Transform3D:
         if self.quaternion:
             return glm.mat4(self.quaternion)
         return euler_rotate_matrix(self.rotation)
-    
+
     @property
     def rotation_quat(self):
         if self.quaternion:
@@ -32,7 +33,7 @@ class Transform3D:
     @property
     def matrix(self):
         return prep_mat(self.glmmatrix)
-        
+
     @property
     def glmmatrix(self):
         if self.swap_rot_order:
@@ -46,21 +47,27 @@ class Transform3D:
             return np.array((self.translate_matrix * self.rotation_matrix * self.scale_matrix).to_list()).T
         else:
             return np.array((self.rotation_matrix * self.translate_matrix * self.scale_matrix).to_list()).T
-        
+
+
 def quat_to_mat(x, y, z, w):
     return np.array(glm.mat4(glm.quat(w, x, y, z)))
+
 
 def flatten(l):
     return [item for sublist in l for item in sublist]
 
+
 def perspective_matrix(fovy, ratio, znear, zfar):
     return glm.perspective(fovy, ratio, znear, zfar)
+
 
 def lookat_matrix(eye, target, up):
     return glm.lookAt(glm.vec3(*eye), glm.vec3(*target), glm.vec3(*up))
 
+
 def prep_mat(matrix):
     return tuple(flatten(matrix.to_tuple()))
+
 
 def euler_rotate_matrix(angles):
     x_mat = glm.rotate(angles[0], glm.vec3(1, 0, 0))
@@ -68,20 +75,24 @@ def euler_rotate_matrix(angles):
     z_mat = glm.rotate(angles[2], glm.vec3(0, 0, 1))
     return x_mat * y_mat * z_mat
 
+
 def change_basis_mat(from_basis, to_basis):
     return np.matmul(np.linalg.inv(np.array(from_basis)), np.array(to_basis))
+
 
 def mat3_to_mat4(mat3):
     new_mat = np.identity(4)
     new_mat[:3, :3] = mat3
     return new_mat
 
+
 def unflatten_matrix(matrix):
     if len(matrix) == 3 * 3:
         return np.array(matrix).reshape(-1, 3)
     elif len(matrix) == 4 * 4:
         return np.array(matrix).reshape(-1, 4)
-    
+
+
 def quat_scale(quat, amount):
     no_rot = glm.quat(1.0, 0.0, 0.0, 0.0)
     if amount <= 1:
@@ -94,12 +105,14 @@ def quat_scale(quat, amount):
     quat_2x = quat * quat
     return glm.mix(no_rot, quat_2x, amount)
 
+
 def vec3_exponent(vec, exp):
     vec = glm.vec3(vec)
     for i in range(3):
         sign = vec[i] / abs(vec[i])
         vec[i] = sign * (abs(vec[i]) ** exp)
     return vec
+
 
 # https://math.stackexchange.com/questions/237369/given-this-transformation-matrix-how-do-i-decompose-it-into-translation-rotati
 def extract_rotation(mat3):
